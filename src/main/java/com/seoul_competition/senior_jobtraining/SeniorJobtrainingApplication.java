80a48c895dcea533575a8b4d21e00e578f321ca6
Beginner <dkstmdwn17@naver.com>
2023-04-15 13:12:01 +0900

    chore: 초기설정
    - 폴더 구조 설정
    - gitignore 설정
    - build.gradle 설정
    
    issue : #1

---
+++ src/main/java/com/seoul_competition/senior_jobtraining/SeniorJobtrainingApplication.java
@@ -0,0 +1,13 @@
+package com.seoul_competition.senior_jobtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeniorJobtrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeniorJobtrainingApplication.class, args);
+	}
+
+}
