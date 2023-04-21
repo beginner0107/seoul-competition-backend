2be7b5e6db92d9acd95f18f1c7f3d28b8658f919
Beginner <dkstmdwn17@naver.com>
2023-04-21 16:46:40 +0900

    feat: 날짜 저장 EnableJpaAuditing 추가
    google formatter 적용도 추가

--- src/main/java/com/seoul_competition/senior_jobtraining/SeniorJobtrainingApplication.java
+++ src/main/java/com/seoul_competition/senior_jobtraining/SeniorJobtrainingApplication.java
@@ -2,12 +2,14 @@ package com.seoul_competition.senior_jobtraining;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SeniorJobtrainingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SeniorJobtrainingApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SeniorJobtrainingApplication.class, args);
+  }
 
 }
