2d82da7aa694357e6d780b66bb559e0957dcda14
Beginner <dkstmdwn17@naver.com>
2023-04-23 01:28:36 +0900

    feat: 게시글 조회 기능 구현

--- src/main/java/com/seoul_competition/senior_jobtraining/domain/post/api/PostController.java
+++ src/main/java/com/seoul_competition/senior_jobtraining/domain/post/api/PostController.java
@@ -2,11 +2,14 @@ package com.seoul_competition.senior_jobtraining.domain.post.api;
 
 import com.seoul_competition.senior_jobtraining.domain.post.application.PostService;
 import com.seoul_competition.senior_jobtraining.domain.post.dto.request.PostSaveReqDto;
+import com.seoul_competition.senior_jobtraining.domain.post.dto.response.PostResDto;
 import jakarta.validation.Valid;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +28,12 @@ public class PostController {
     return ResponseEntity.status(HttpStatus.CREATED)
         .location(URI.create("/posts/" + postId))
         .build();
+  }
+
+  @GetMapping("{postId}")
+  public ResponseEntity<PostResDto> getPost(@PathVariable Long postId) {
+    PostResDto postResDto = postService.getPost(postId);
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(postResDto);
   }
 }
--- src/main/java/com/seoul_competition/senior_jobtraining/domain/post/dao/PostRepository.java
+++ src/main/java/com/seoul_competition/senior_jobtraining/domain/post/dao/PostRepository.java
@@ -1,8 +1,12 @@
 package com.seoul_competition.senior_jobtraining.domain.post.dao;
 
 import com.seoul_competition.senior_jobtraining.domain.post.entity.Post;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+  @EntityGraph(attributePaths = "comments")
+  Optional<Post> findById(Long id);
 }
