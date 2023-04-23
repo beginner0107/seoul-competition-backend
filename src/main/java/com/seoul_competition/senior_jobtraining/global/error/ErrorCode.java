83c4f942202476f4bb7e03f30d037804e2d4cecf
Beginner <dkstmdwn17@naver.com>
2023-04-23 23:31:40 +0900

    feat: 비밀번호 불일치 시 발생할 에러 작성

--- src/main/java/com/seoul_competition/senior_jobtraining/global/error/ErrorCode.java
+++ src/main/java/com/seoul_competition/senior_jobtraining/global/error/ErrorCode.java
@@ -5,8 +5,9 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
-  
-  POST_NOT_EXISTS("해당 게시글은 존재하지 않습니다.", HttpStatus.BAD_REQUEST);
+
+  POST_NOT_EXISTS("해당 게시글은 존재하지 않습니다.", HttpStatus.BAD_REQUEST),
+  PASSWORD_MISMATCH("비밀번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST);
 
   private String message;
   private HttpStatus httpStatus;
