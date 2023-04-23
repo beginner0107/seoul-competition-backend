2ee608a427d4ba859313f2065483e2553728cc5e
kawksung <alstjd5479@naver.com>
2023-04-23 11:17:08 +0900

    feat: 교육 목록과 세부 내용 조회 구현

---
+++ src/main/java/com/seoul_competition/senior_jobtraining/domain/education/dto/response/EducationResponse.java
@@ -0,0 +1,30 @@
+package com.seoul_competition.senior_jobtraining.domain.education.dto.response;
+
+import com.seoul_competition.senior_jobtraining.domain.education.entity.EducationEntity;
+import lombok.Getter;
+
+
+@Getter
+public class EducationResponse {
+
+  private String name;
+  private String state;
+  private String url;
+  private int price;
+  private String registerStart;
+  private String registerEnd;
+  private String educationStart;
+  private String educationEnd;
+
+  public EducationResponse(EducationEntity education) {
+    this.name = education.getName();
+    this.state = education.getState();
+    this.url = education.getUrl();
+    this.price = education.getPrice();
+    this.registerStart = education.getRegisterStart();
+    this.registerEnd = education.getRegisterEnd();
+    this.educationStart = education.getEducationStart();
+    this.educationEnd = education.getEducationEnd();
+  }
+
+}
