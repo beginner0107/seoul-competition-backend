c1de0afacefee5c46fcb0dad0f44c50f69b64b33
Beginner <dkstmdwn17@naver.com>
2023-04-23 18:30:49 +0900

    fix: 게시글 저장 에러 처리 구현

--- src/main/java/com/seoul_competition/senior_jobtraining/global/error/ExceptionAdvice.java
+++ src/main/java/com/seoul_competition/senior_jobtraining/global/error/ExceptionAdvice.java
@@ -1,9 +1,11 @@
 package com.seoul_competition.senior_jobtraining.global.error;
 
+import java.util.Map;
 import java.util.stream.Collectors;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,25 +13,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ExceptionAdvice {
 
   @ExceptionHandler(BindException.class)
-  public ResponseEntity<ErrorResponse> bindException(BindException e) {
-    String errorMessage = getErrorMessage(e);
-    return ResponseEntity.badRequest()
-        .body(ErrorResponse.from(errorMessage));
+  public ResponseEntity<Map<String, String>> bindException(BindException e) {
+    Map<String, String> errors = getErrorMessage(e.getBindingResult());
+    return ResponseEntity.badRequest().body(ErrorResponse.from(errors));
   }
 
-  private static String getErrorMessage(BindException e) {
-    BindingResult bindingResult = e.getBindingResult();
-
-    return bindingResult.getFieldErrors()
-        .stream()
-        .map(fieldError -> getErrorMessage((String) fieldError.getRejectedValue(),
-            fieldError.getField(),
-            fieldError.getDefaultMessage()))
-        .collect(Collectors.joining(", "));
-  }
-
-  public static String getErrorMessage(String invalidValue, String errorField,
-      String errorMessage) {
-    return String.format("[%s] %s: %s", errorField, invalidValue, errorMessage);
+  private static Map<String, String> getErrorMessage(BindingResult bindingResult) {
+    return bindingResult.getFieldErrors().stream()
+        .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
   }
 }
