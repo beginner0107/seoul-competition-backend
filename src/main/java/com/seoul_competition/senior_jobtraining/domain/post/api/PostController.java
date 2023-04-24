f72e441c7ef674780ef2fccd28a244c5a7887c49
Beginner <dkstmdwn17@naver.com>
2023-04-24 18:45:36 +0900

    fix: api 규격 변경

--- src/main/java/com/seoul_competition/senior_jobtraining/domain/post/api/PostController.java
+++ src/main/java/com/seoul_competition/senior_jobtraining/domain/post/api/PostController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/posts")
+@RequestMapping("/api/v1/posts")
 public class PostController {
 
   private final PostService postService;
