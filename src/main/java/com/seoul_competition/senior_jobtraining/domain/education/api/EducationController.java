cdc2e925afcff1cb20588d8643a39de73e946121
kawksung <alstjd5479@naver.com>
2023-04-27 22:50:25 +0900

    feat: 검색 기능 구현

--- src/main/java/com/seoul_competition/senior_jobtraining/domain/education/api/EducationController.java
+++ src/main/java/com/seoul_competition/senior_jobtraining/domain/education/api/EducationController.java
@@ -1,8 +1,9 @@
 package com.seoul_competition.senior_jobtraining.domain.education.api;
 
 import com.seoul_competition.senior_jobtraining.domain.education.application.EducationService;
-import com.seoul_competition.senior_jobtraining.domain.education.dto.response.EducationListResponse;
+import com.seoul_competition.senior_jobtraining.domain.education.dto.response.EducationListPageResponse;
 import com.seoul_competition.senior_jobtraining.domain.education.dto.response.EducationResponse;
+import com.seoul_competition.senior_jobtraining.domain.education.dto.response.EducationSearchListResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
@@ -11,7 +12,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,13 +27,20 @@ public class EducationController {
   private boolean first = true;
 
   @GetMapping
-  public ResponseEntity<EducationListResponse> getAllEducations(
+  public ResponseEntity<EducationListPageResponse> getAllEducations(
       @PageableDefault(sort = "state", size = 20, direction = Direction.DESC) Pageable pageable) {
     if (first) {
       educationService.saveAll();
       first = false;
     }
-    EducationListResponse response = educationService.findAllByPage(pageable);
+    EducationListPageResponse response = educationService.findAllByPage(pageable);
+    return ResponseEntity.status(HttpStatus.OK).body(response);
+  }
+
+  @GetMapping("/search")
+  public ResponseEntity<EducationSearchListResponse> getSearchEducations(
+      @RequestParam("name") String name) {
+    EducationSearchListResponse response = educationService.findAllByName(name);
     return ResponseEntity.status(HttpStatus.OK).body(response);
   }
 
--- src/main/java/com/seoul_competition/senior_jobtraining/domain/education/dao/EducationRepository.java
+++ src/main/java/com/seoul_competition/senior_jobtraining/domain/education/dao/EducationRepository.java
@@ -1,8 +1,13 @@
 package com.seoul_competition.senior_jobtraining.domain.education.dao;
 
 import com.seoul_competition.senior_jobtraining.domain.education.entity.Education;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface EducationRepository extends JpaRepository<Education, Long> {
 
+  @Query("SELECT e FROM Education e WHERE e.name LIKE %:name% ORDER BY e.state DESC")
+  List<Education> fs(@Param("name") String name);
 }
