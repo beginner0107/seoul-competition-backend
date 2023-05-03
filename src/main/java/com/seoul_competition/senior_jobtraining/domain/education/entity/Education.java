641f3e83d959d5d5db98b8cba5426ae9f1c5bff4
kawksung <alstjd5479@naver.com>
2023-05-03 23:12:45 +0900

    feat: 교육 엔티티의 생성시간 구현

--- src/main/java/com/seoul_competition/senior_jobtraining/domain/education/entity/Education.java
+++ src/main/java/com/seoul_competition/senior_jobtraining/domain/education/entity/Education.java
@@ -4,22 +4,27 @@ import com.seoul_competition.senior_jobtraining.domain.review.entity.Review;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.lang.String;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "education")
 public class Education {
 
@@ -52,6 +57,10 @@ public class Education {
 
   @OneToMany(mappedBy = "education", cascade = CascadeType.REMOVE)
   private List<Review> reviews = new ArrayList<>();
+
+  @CreatedDate
+  @Column(nullable = false)
+  private LocalDateTime createdAt;
 
   @Builder
   public Education(String name, String state, String url, String price, int capacity,
