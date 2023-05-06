a6a3a27737c8d66b9e0bfa538b9836b41509e83b
kawksung <alstjd5479@naver.com>
2023-05-06 14:30:34 +0900

    feat: 교육의 상세보기에 댓글 내용 추가

--- src/main/java/com/seoul_competition/senior_jobtraining/domain/education/dto/response/EducationDetailResDto.java
+++ src/main/java/com/seoul_competition/senior_jobtraining/domain/education/dto/response/EducationDetailResDto.java
@@ -1,8 +1,10 @@
 package com.seoul_competition.senior_jobtraining.domain.education.dto.response;
 
 import com.seoul_competition.senior_jobtraining.domain.education.entity.Education;
+import com.seoul_competition.senior_jobtraining.domain.review.dto.response.ReviewResDto;
 import com.seoul_competition.senior_jobtraining.domain.review.entity.Review;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public record EducationDetailResDto(
     Long id,
@@ -16,13 +18,14 @@ public record EducationDetailResDto(
     String educationStart,
     String educationEnd,
     String url,
-    Long hits) {
+    Long hits,
+    List<ReviewResDto> reviews) {
 
   public EducationDetailResDto(Education education) {
     this(education.getId(), education.getName(), education.getState(), education.getPrice(),
         education.getCapacity(), education.getRegisterStart(), education.getRegisterEnd(),
         education.getEducationStart(), education.getEducationEnd(), education.getUrl(),
-        education.getHits());
+        education.getHits(),
+        education.getReviews().stream().map(ReviewResDto::new).collect(Collectors.toList()));
   }
-
 }
