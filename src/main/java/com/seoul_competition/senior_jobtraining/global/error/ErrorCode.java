595fb2ba27b975ca8d55605c8615c7154a693f6f
kawksung <alstjd5479@naver.com>
2023-05-06 15:00:26 +0900

    conflict: 충돌 해결

--- src/main/java/com/seoul_competition/senior_jobtraining/global/error/ErrorCode.java
+++ src/main/java/com/seoul_competition/senior_jobtraining/global/error/ErrorCode.java
@@ -10,9 +10,9 @@ public enum ErrorCode {
   COMMENT_NOT_EXISTS("해당 댓글은 존재하지 않습니다.", HttpStatus.BAD_REQUEST),
   PASSWORD_MISMATCH("비밀번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST),
   PAGE_NOT_FOUND("페이지를 찾을 수 없습니다.", HttpStatus.BAD_REQUEST),
-  PASSWORD_EMPTY("비밀번호를 입력해주세요", HttpStatus.BAD_REQUEST),
   EDUCATION_NOT_EXISTS("해당 교육은 존재하지 않습니다.", HttpStatus.BAD_REQUEST),
-  REVIEW_NOT_EXISTS("해당 리뷰는 존재하지 않습니다.", HttpStatus.BAD_REQUEST);
+  REVIEW_NOT_EXISTS("해당 리뷰는 존재하지 않습니다.", HttpStatus.BAD_REQUEST),
+  PASSWORD_EMPTY("비밀번호를 입력해주세요", HttpStatus.BAD_REQUEST);
 
   private String message;
   private HttpStatus httpStatus;
